a3267f5ffb4c057f1a331a71e31aa0191b82dd35
Sam Pluta <spluta1@sam.local>
2025-09-21 11:20:14 -0400

    priority mode

--- examples/many_oscillators.py
+++ examples/many_oscillators.py
@@ -22,27 +22,10 @@ mmm_audio.stop_audio() # stop/pause the audio thread
 # if you are expericencing audio dropouts, these can be avoided by giving python the highest "nice" priority
 # unfortunately this can't be done from the terminal that vscode automatically launches
 # it can only be done from a user made terminal
-# from a new terminal window and from the MMMAudio directory run "sudo venv/bin/python" to run python in sudo mode
-# then copy and paste your code, like the example below (adjusting the nice value below 0), into the terminal repl to run it
+# from a new terminal window and from the MMMAudio directory run "sudo nice -n -20 venv/bin/python" to run python in sudo mode with highest priority
+# then copy and paste your code, like the example below, into the terminal repl to run it
 
 from mmm_src.MMMAudio import MMMAudio
-
-import os
-import sys
-import psutil
-
-# Get the current process ID
-process = psutil.Process(os.getpid())
-process.nice()
-
-# Set a custom nice value.
-# The range is -20 (high priority) to 19 (low priority) on Linux/macOS.
-# Values depend on the OS for Windows.
-try:
-    process.nice(-20) # Set to a higher priority
-    print(f"Set process priority to: {process.nice()}")
-except (psutil.AccessDenied, psutil.NoSuchProcess):
-    print("Could not change process priority.")
 
 mmm_audio = MMMAudio(128, graph_name="ManyOscillators", package_name="examples")
 
