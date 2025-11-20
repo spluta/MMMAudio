361eae5b022cb7b2d0ecb16e833fe33781943638
spluta <spluta@gmail.com>
2025-11-19 19:33:01 -0800

    back to old stereo fft

--- examples/spectral_freeze_example.py
+++ examples/spectral_freeze_example.py
@@ -1,12 +1,37 @@
+import sys
+from pathlib import Path
+
+# This example is able to run by pressing the "play" button in VSCode
+# that executes the whole file.
+# In order to do this, it needs to add the parent directory to the path
+# (the next line here) so that it can find the mmm_src and mmm_utils packages.
+# If you want to run it line by line in a REPL, skip this line!
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
 from mmm_src.MMMAudio import MMMAudio
-from mmm_src.Patterns import Pseq
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton
+
 
 mmm_audio = MMMAudio(256, graph_name="SpectralFreezeExample", package_name="examples")
 mmm_audio.start_audio()
 
-# make a sequence that toggles the freeze gate on and off
-seq = Pseq([True, False])
+app = QApplication([])
 
-mmm_audio.send_bool("freeze_gate", seq.next())
+# Create the main window
+window = QWidget()
+window.setWindowTitle("Spectral Freeze Controller")
+window.resize(100, 100)
+# stop audio when window is closed
+window.closeEvent = lambda event: (mmm_audio.stop_audio(), event.accept())
 
-mmm_audio.stop_audio()
+# Create layout
+layout = QVBoxLayout()
+
+gatebutton = QPushButton("Freeze Gate")
+gatebutton.setCheckable(True)
+gatebutton.setChecked(False)
+gatebutton.clicked.connect(lambda checked: mmm_audio.send_bool("freeze_gate", checked))
+layout.addWidget(gatebutton)
+window.setLayout(layout)
+window.show()
+app.exec()
