e59e68ec1b1010bd9b25b4e4908fdf279a27176d
spluta <spluta@gmail.com>
2026-01-20 11:17:59 -0500

    simply add mmm_python

---
+++ mmm_python/__init__.py
@@ -0,0 +1,8 @@
+from mmm_python.MMMAudio import *
+from mmm_python.python_utils import *
+from mmm_python.Patterns import *
+import asyncio
+from mmm_python.OSCServer import *
+from mmm_python.Scheduler import *
+from mmm_python.GUI import *
+from mmm_python.hid_devices import *
